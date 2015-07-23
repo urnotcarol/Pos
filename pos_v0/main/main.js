aed849a57a4d42bb4dab99a2bb7f165c490d1eaf
Chrissie <urnotchrissie@outlook.com>
2015-07-23 15:46:49 +0800

    优化（收据三段式输出）

--- pos_v0/main/main.js
+++ pos_v0/main/main.js
@@ -1,17 +1,24 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
   var total_price = 0;
-  for (var i = 0; i < inputs.length; i++) {
-    total_price += inputs[i].price * inputs[i].count;
-  }
-  var outputs = "***<没钱赚商店>收据***" + '\n' +
-  "名称：" + inputs[0].name +  "，数量：" + inputs[0].count + inputs[0].unit +
-  "，单价：" + inputs[0].price.toFixed(2) + "(元)，小计：" + (inputs[0].price * inputs[0].count).toFixed(2) + "(元)" + '\n' +
-  "名称：" + inputs[1].name +  "，数量：" + inputs[1].count + inputs[1].unit +
-  "，单价：" + inputs[1].price.toFixed(2) + "(元)，小计：" + (inputs[1].price * inputs[1].count).toFixed(2) + "(元)" + '\n' +
-  "名称：" + inputs[2].name +  "，数量：" + inputs[2].count + inputs[2].unit +
-  "，单价：" + inputs[2].price.toFixed(2) + "(元)，小计：" + (inputs[2].price * inputs[2].count).toFixed(2) + "(元)" + '\n' +
-  "----------------------" + '\n' + "总计：" + total_price.toFixed(2) + "(元)" + '\n' +
-  "**********************";
+
+  inputs.forEach(function(val) {
+    total_price += val.price * val.count;
+  });
+
+  var outputs = "***<没钱赚商店>收据***" + '\n';
+
+  inputs.forEach(function(val) {
+    outputs +=
+    "名称：" + val.name + "，" +
+    "数量：" + val.count + val.unit + "，" +
+    "单价：" + val.price.toFixed(2) + "(元)，" +
+    "小计：" + (val.price * val.count).toFixed(2) + "(元)" + '\n';
+  });
+
+  outputs +=  "----------------------" + '\n';
+  outputs +=  "总计：" + total_price.toFixed(2) + "(元)" + '\n';
+  outputs +=  "**********************";
+
   console.log(outputs);
 }
